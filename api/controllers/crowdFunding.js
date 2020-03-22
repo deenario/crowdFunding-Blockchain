90198d33cd80cb2c43c4cde59d323c1163628fb2
Danyal Ahmad <danyal20098@gmail.com>
2020-03-22 14:58:19 +0500

    voting apis fixed

--- api/controllers/crowdFunding.js
+++ api/controllers/crowdFunding.js
@@ -100,6 +100,7 @@ exports.queryChat = async (req, res) => {
 };
 
 exports.createVote = async (req, res) => {
+    console.log(req.body);
     let vote = {
         voteID: req.body.voteID ? req.body.voteID : " ",
         projectID: req.body.projectID ? req.body.projectID : " ",
@@ -114,6 +115,7 @@ exports.queryVotebyID = async (req, res) => {
     if (!req.query.voteID) {
         return res.status(404).json({statusCode: 404, message: "Provide voteID!"});
     }
+    console.log(req.query);
     let result = await fabric.contract.evaluateTransaction('queryVotebyID', req.query.voteID);
     res.status(200).json({statusCode: 200, data: JSON.parse(result.toString())});
 };
@@ -122,6 +124,7 @@ exports.queryVotebyProject = async (req, res) => {
     if (!req.query.projectID) {
         return res.status(404).json({statusCode: 404, message: "Provide projectID!"});
     }
+    console.log(req.query);
     let result = await fabric.contract.evaluateTransaction('queryVotebyProject', req.query.projectID);
     res.status(200).json({statusCode: 200, data: JSON.parse(result.toString())});
 };
